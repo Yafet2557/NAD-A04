51ad4889213d568b622abe36ab3af33824f14ca1
Yafet2557 <ytekleab2557@conestogac.on.ca>
2024-03-25 00:48:57 -0400

    Adding a form to the modal with crispy form

--- src/posts_proj/settings.py
+++ src/posts_proj/settings.py
@@ -43,8 +43,14 @@ INSTALLED_APPS = [
     'profiles',
 
     # Third party apps
-    'crispy_forms'
+    'crispy_forms',
+    'crispy_bootstrap4',
+    
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
