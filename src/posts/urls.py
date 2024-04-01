02580622eaa1d74523766dcf4da5298801a61da4
Yafet2557 <ytekleab2557@conestogac.on.ca>
2024-04-01 16:06:53 -0400

    Creating view for image uploading

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -6,7 +6,8 @@ from .views import(
     post_detail,
     post_detail_data_view,
     delete_post,
-    update_post
+    update_post,
+    image_upload_view
 )
 
 app_name = 'posts'
@@ -15,6 +16,9 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('data/<int:num_posts>/', load_posts_data_view, name="posts-data"),
     path('like-unlike/', like_unlike_post, name="like-unlike"),
+    
+    path('upload/', image_upload_view, name="image-upload"),
+    
     path('<pk>/', post_detail, name="post-detail"),
     path('<pk>/data/', post_detail_data_view, name="post-detail-data"),
     path('<pk>/delete/', delete_post, name="post-delete"),
